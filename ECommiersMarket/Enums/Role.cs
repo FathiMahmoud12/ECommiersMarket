31b30d871edc1d14c692d89d1552bcafe9707570
FathiM09 <116751263+FathiM09@users.noreply.github.com>
2023-06-26 16:56:26 +0200

    Add project files.

---
+++ ECommiersMarket/Enums/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstSelection.Enums
+{
+    [Flags]
+
+    public enum Role
+    {
+        SystemAdmin = 1,
+    }
+
+}

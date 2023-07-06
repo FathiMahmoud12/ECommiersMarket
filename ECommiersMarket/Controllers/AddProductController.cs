e9e5e0ee86b7ac94dbe015087fc34f1871459aaf
Fathi <Fathi@DESKTOP-C1376H0>
2023-07-06 12:51:27 +0300

    Commit

---
+++ ECommiersMarket/Controllers/AddProductController.cs
@@ -0,0 +1,33 @@
+﻿using ECommiersMarket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommiersMarket.Controllers
+{
+    public class AddProductController : Controller
+    {
+        ZahraMarketEntities db = new ZahraMarketEntities();
+        // GET: AddProduct
+        public ActionResult Index()
+        {
+            var Combo_Iteams = db.MainGroups.ToList();
+
+            if (Combo_Iteams != null)
+            {
+                @ViewBag.data = Combo_Iteams;
+            }
+            @ViewBag.SupGroup = new SelectList("");
+
+            return View();
+        }
+        public ActionResult getCats(int Id)
+        {
+            var model = db.SubGroupsTs.Where(x => x.GroupID == Id).Select(x => new { x.Id, x.SubGroupName }).ToList();
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}

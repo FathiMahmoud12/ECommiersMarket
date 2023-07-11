9f4383d33b47d6ca8d222a69dd57ea7be57632f5
FathiM09 <116751263+FathiM09@users.noreply.github.com>
2023-07-11 13:47:13 +0200

    coom

---
+++ ECommiersMarket/Controllers/AddStoreController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommiersMarket.Controllers
+{
+    public class AddStoreController : Controller
+    {
+        // GET: AddStore
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
